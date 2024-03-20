7e5f045d8c180c1c5edd303d03b9d58fd3aa72fb
Lea Antony <tonimemangkeren76@gmail.com>
2024-03-20 09:29:03 +0700

    perbaiki route

--- Praktikum-6/routes/web.php
+++ Praktikum-6/routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/listbarang/{id}/{nama}', [ListProductController::class, 'tampilkan']);
+Route::get('/list_product/{id}/{nama}', [ListProductController::class, 'tampilkan']);
 Route::get('/list_product', [ListProductController::class, 'tampilkan']);
