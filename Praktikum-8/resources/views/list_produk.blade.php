494f942e5cba37d0a7340d45c1c9e107b6290dd2
Lea Antony <tonimemangkeren76@gmail.com>
2024-05-17 11:02:53 +0700

    Update praktium 8

--- Praktikum-8/resources/views/list_produk.blade.php
+++ Praktikum-8/resources/views/list_produk.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en" data-theme="luxury">
+<html lang="en" data-theme="cyberpunk">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Praktikum 8</title>
     <link rel="stylesheet" href="/css/daisyui/daisyui.min.css">
     <script src="/js/daisyui/daisyui.min.js"></script>
 </head>
